f441eab21c2f7c8385371f08ba27cab4bec818a8
buhuishiwo <1437317989@qq.com>
2025-04-26 00:59:58 +0800

    update

--- springboot-06-security/src/main/java/com/haishili/config/SecurityConfig.java
+++ springboot-06-security/src/main/java/com/haishili/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.haishili.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,7 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
 
 /**
  * @author haishili
@@ -43,8 +45,16 @@ public class SecurityConfig  {
                 )
                 //此方法已被废弃
                 //.logout();
+                .csrf(AbstractHttpConfigurer::disable)
+
                 .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
-                        .logoutSuccessUrl("/"));
+                        .logoutSuccessUrl("/")
+                )
+                .rememberMe(httpSecurityRememberMeConfigurer -> httpSecurityRememberMeConfigurer
+                        .rememberMeParameter("remember")
+                        .rememberMeCookieName("remember")
+                        .userDetailsService(userDetailsService())
+                );
         return http.build();
     }
 
--- springboot-06-security/src/main/java/com/haishili/controller/RouterController.java
+++ springboot-06-security/src/main/java/com/haishili/controller/RouterController.java
@@ -15,18 +15,22 @@ public class RouterController {
     public String index() {
         return "index";
     }
+
     @RequestMapping("/login")
     public String toLogin() {
         return "views/login";
     }
+
     @RequestMapping("/level1/{id}")
     public String view1(@PathVariable("id") int id) {
         return "views/level1/"+id;
     }
+
     @RequestMapping("/level2/{id}")
     public String view2(@PathVariable("id") int id) {
         return "views/level2/"+id;
     }
+
     @RequestMapping("/level3/{id}")
     public String view3(@PathVariable("id") int id) {
         return "views/level3/"+id;
